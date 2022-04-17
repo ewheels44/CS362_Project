d3d873dee9f1613af595f2d5984a87df9b9d21e0
Ethan Wheeler <ethanwheeler@Ethans-MacBook-Pro.local>
2022-04-17 12:51:45 -0500

    UPDATE: added tweaks

--- tension.py
+++ tension.py
@@ -1,27 +1,16 @@
 #!/usr/bin/env python3
 
-"""Control an Arduino over the USB port."""
-
-# usb.py
-# Created by John Woolsey on 12/17/2019.
-# Copyright (c) 2019 Woolsey Workshop.  All rights reserved.
-#
-#
-# Edits by: Ethan Wheeler
-#
-
-
-# USB_PORT = "/dev/ttyUSB0"  # Arduino Uno R3 Compatible
 USB_PORT = "/dev/ttyACM0"  # Arduino Uno WiFi Rev2
 
-
-# Imports
 import serial
 
 
-# Functions
-def print_commands():
-   """Prints available commands."""
+#
+# print_commands
+#
+# prints available comands to user
+#
+def help():
    print("Sailing Preformance Limited")
    print("Welcome")
    print("Starting up....")
@@ -31,32 +20,42 @@ def print_commands():
    print("  s - start monitoring tension")
    print("  x - Exit program")
 
+#
+# tension
+#
+# reads data frmo the Arduino
+#
+# def read_tension(s):
+#     while True:
+#         stop = input("press e to stop: ")
 
-# Main
 
-# Connect to USB serial port at 9600 baud
+
+###########################################
+#               Main
+
+# Try ad connect to USB serial port at 9600
 try:
    usb = serial.Serial(USB_PORT, 9600, timeout=2)
 except:
-   print("ERROR - Could not open USB serial port.  Please check your port name and permissions.")
+   print("ERROR!!!")
    print("Exiting program.")
    exit()
 
-# Send commands to Arduino
-print("Enter a command from the keyboard to send to the Arduino.")
-print_commands()
+help()
 
 while True:
    command = input("Enter command: ")
-   if command == "a":  # read Ardino A0 pin value
+   if command == "a":       # fail safe test
     print("fail safe")
    elif command == "s":
-      usb.write(b' read_tension')
-      line = usb.readline()  # read
-      line = line.decode()  # convert 
-      line = line.strip()  # strip extra whitespace characters
+      while (command == "s"):
+          usb.write(b' read_tension')
+          line = usb.readline()  # read
+          line = line.decode()  # convert 
+          line = line.strip()  # strip extra whitespace characters
 
-      print(line)
+          print(line)
 
    elif command == "x":  # exit program
       print("Exiting program.")
